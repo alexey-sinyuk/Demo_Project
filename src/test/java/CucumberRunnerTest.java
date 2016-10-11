a9019066f6da5e8929b9ef046e69828ea566e6de
asinuk-sinyuk <sinuykal@gmail.com>
2016-10-11 15:51:37 +0300

    added json reporting from cucumber

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -2,11 +2,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-
-
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = false, features = "src/test/java/features", format = { "pretty",
-        "html:target/cucumber_reports" })
+@CucumberOptions(strict = false, features = "src/test/java/features",
+        format = {"json:target/cucumber_reports/cucumber.json", "pretty", "html:target/cucumber_reports"})
 
 public class CucumberRunnerTest {
 }
